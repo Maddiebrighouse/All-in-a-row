b2131fcc1b6b571728fd0745aaac5ca69130886d
madeleine-brighouse <31881309+Maddiebrighouse@users.noreply.github.com>
2018-02-05 21:11:22 +1300

    trying to use react

---
+++ clent/components/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import FixedNavagation from './Navagation'
+
+class App extends React.Component {
+    render() {
+
+        return (
+            <div>
+                <FixedNavagation />
+            </div>
+        )
+    }
+}
+
+export default Apps
