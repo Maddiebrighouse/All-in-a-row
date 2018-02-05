2e6b0a74ce1760fc5d9967cce56b8f9b502166b9
madeleine-brighouse <31881309+Maddiebrighouse@users.noreply.github.com>
2018-02-05 18:25:09 +1300

    added webpack

---
+++ clent/App.jsx
@@ -0,0 +1,12 @@
+
+class MainHeader extends React.Component {
+    render() {
+        return (
+            <div className='header'>
+                <img class="header" src="/images/All-in-a-row-header.jpg" alt="Header Banner"/>
+            </div>
+                );
+    }
+  }
+
+  export default MainHeader
