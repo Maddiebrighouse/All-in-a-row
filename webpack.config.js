3421f8bf49ee79cc657e554ac2c1c9c3c0ae4785
madeleine-brighouse <31881309+Maddiebrighouse@users.noreply.github.com>
2019-03-18 17:57:36 +1300

    updated everything to newest verion except node left at 5.0.0 as worked best

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,45 @@
+// eslint-disable-next-line no-undef
 const path = require('path')
 
+// eslint-disable-next-line no-undef
 module.exports = {
+  mode: 'development',
   entry: './client/index.js',
   output: {
     path: path.join(__dirname, 'public'),
     filename: 'bundle.js'
   },
   module: {
-    loaders: [
-      {
-        test: /\.css$/,
-        loader: 'style-loader!css-loader'
-      }
-    ],
+    // loaders: [{
+    //     test: /\.css$/,
+    //     loader: 'style-loader!css-loader'
+    //   },
+    //   {
+    //     test: /\.scss$/,
+    //     loader: 'style-loader!sass-loader'
+    //   }
+    // ],
     rules: [{
-      test: /\.css$|\.scss$|\.sass$/,
-      use: [
-        {loader: 'style-loader'},
-        {loader: 'css-loader'},
-        {loader: 'sass-loader'}
-      ]
-    }, {
-      test: /\.jsx?$/,
-      exclude: /node_modules/,
-      use: [
-        {loader: 'babel-loader'}
-      ]
-    }]
+        test: /\.css$/,
+        use: 'style-loader!css-loader'
+      },
+      {
+        test: /\.scss$/,
+        use: 'style-loader!sass-loader'
+      },
+      {
+        test: /\.sass$/,
+        use: 'style-loader!sass-loader'
+      },
+
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: [{
+          loader: 'babel-loader'
+        }]
+      }
+    ]
   },
   resolve: {
     extensions: ['.js', '.jsx']
