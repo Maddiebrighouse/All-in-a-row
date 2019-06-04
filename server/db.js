b0ea340a2fb25240edff2da9b18542eccbac70d3
madeleine-brighouse <31881309+Maddiebrighouse@users.noreply.github.com>
2019-06-04 13:52:32 +1200

    Remove Knex from app as not being used

--- server/db.js
+++ server/db.js
@@ -1,17 +1,3 @@
-// const knex = require('knex')
-// const config = require('../knexfile').development
-
-// const devdb = knex(config)
-
-// function getPosts (testDb) {
-//     const db = testDb || devdb
-//     return db('entries')
-//       .select()
-//   }
-
-//   module.exports = {
-//       getPosts
-//   }
 require('dotenv').config();
 const MongoClient = require('mongodb').MongoClient;
 
